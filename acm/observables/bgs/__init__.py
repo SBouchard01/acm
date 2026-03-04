f293242a8d37e1769787671b500d1c47df4ff32f
SBouchard01 <simonbouchard47@gmail.com>
2026-03-04 07:04:20 -0800

    Add Fourier-space BGS observables

--- acm/observables/bgs/__init__.py
+++ acm/observables/bgs/__init__.py
@@ -1,8 +1,13 @@
 from .tpcf_module import tpcf
-from .density_split_correlation import ds_xiqg, ds_xiqq
+from .power_spectrum import spectrum
+from .density_split_correlation import ds_xiqq, ds_xiqg
+from .density_split_spectrum import ds_pkqq, ds_pkqg
 
 __all__ = [
-    "tpcf_module",
+    "tpcf",
+    "spectrum",
     "ds_xiqg",
     "ds_xiqq",
+    "ds_pkqg",
+    "ds_pkqq",
 ]
